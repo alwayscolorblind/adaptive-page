f4832609c27924df3570bc11310c5a4a8f5e6f99
realpho3nix <55889540+realpho3nix@users.noreply.github.com>
2021-07-31 20:08:44 +0300

    Added new section

--- js/slider.js
+++ js/slider.js
@@ -1,52 +1,58 @@
-const buttonNext = document.querySelector('.slider__button_direction_right');
-const buttonPrev = document.querySelector('.slider__button_direction_left');
-const itemCount = document.querySelectorAll('.slider__item-link').length;
-const paginationBullets = document.querySelectorAll('.slider__pagination-bullet');
-let currentItemIndex = 0;
+const sliders = document.querySelectorAll('.slider');
 
-setActivePaginationBullet(currentItemIndex);
-
-buttonNext.addEventListener('click', () => {
-    changeSlide('next');
-});
-
-buttonPrev.addEventListener('click', () => {
-    changeSlide('prev');
-});
-
-paginationBullets.forEach((bullet, index) => {
-    bullet.addEventListener('click', () => {
-        changeSlideByIndex(index);
-    });
-})
-
-function changeSlide(direction) {
-    if (direction === 'next') {
-        currentItemIndex++;
-        if (currentItemIndex === itemCount) {
-            currentItemIndex = 0;
-        }
-    }
-    if (direction === 'prev') {
-        currentItemIndex--;
-        if (currentItemIndex < 0) {
-            currentItemIndex = itemCount - 1;
-        }
-    }
+sliders.forEach(slider => {
+    const buttonNext = slider.querySelector('.slider__button_direction_right');
+    const buttonPrev = slider.querySelector('.slider__button_direction_left');
+    const itemCount = slider.querySelectorAll('.slider__item-link').length;
+    const paginationBullets = slider.querySelectorAll('.slider__pagination-bullet');
+    let currentItemIndex = 0;
 
     setActivePaginationBullet(currentItemIndex);
-    document.querySelector('.slider__wrapper').style.transform = `translateX(-${currentItemIndex * 100}%)`;
-}
 
-function setActivePaginationBullet(index) {
-    paginationBullets.forEach(bullet => {
-        bullet.classList.remove('slider__pagination-bullet_state_active');
+    buttonNext.addEventListener('click', (event) => {
+        event.preventDefault();
+        changeSlide('next', itemCount);
     });
 
-    paginationBullets[index].classList.add('slider__pagination-bullet_state_active');
-}
+    buttonPrev.addEventListener('click', (event) => {
+        event.preventDefault();
+        changeSlide('prev', itemCount);
+    });
 
-function changeSlideByIndex(index) {
-    document.querySelector('.slider__wrapper').style.transform = `translateX(-${index * 100}%)`;
-    setActivePaginationBullet(index);
-}
+    paginationBullets.forEach((bullet, index) => {
+        bullet.addEventListener('click', () => {
+            changeSlideByIndex(index);
+        });
+    })
+
+    function changeSlide(direction, itemCount) {
+        if (direction === 'next') {
+            currentItemIndex++;
+            if (currentItemIndex === itemCount) {
+                currentItemIndex = 0;
+            }
+        }
+        if (direction === 'prev') {
+            currentItemIndex--;
+            if (currentItemIndex < 0) {
+                currentItemIndex = itemCount - 1;
+            }
+        }
+
+        setActivePaginationBullet(currentItemIndex);
+        slider.querySelector('.slider__wrapper').style.transform = `translateX(-${currentItemIndex * 100}%)`;
+    }
+
+    function setActivePaginationBullet(index) {
+        paginationBullets.forEach(bullet => {
+            bullet.classList.remove('slider__pagination-bullet_state_active');
+        });
+
+        paginationBullets[index].classList.add('slider__pagination-bullet_state_active');
+    }
+
+    function changeSlideByIndex(index) {
+        slider.querySelector('.slider__wrapper').style.transform = `translateX(-${index * 100}%)`;
+        setActivePaginationBullet(index);
+    }
+});
